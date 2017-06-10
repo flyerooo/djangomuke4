831538ffedcb6bd5206a6f56310ae9fe29a36fa2
flyerooo <wxl.pure@gmail.com>
2017-06-10 13:03:24 +0800

    用类LoginView重写用户登录user_login

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
 
@@ -16,6 +17,20 @@ class CustomBackend(ModelBackend):
                 return user
         except Exception as e:
             return None
+
+
+class LoginView(View):
+    def get(self,request):
+        return render(request, "login.html", {})
+    def post(self,request):
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(username=user_name, password=pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, "index.html")
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误!"})
 
 
 # Create your views here.
